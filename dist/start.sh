a78743ba7b87ade48d41f9a4413a458bc38875fd
Mayol <mperrin@feelingsports.com>
2025-09-26 10:46:22 +0200

    DELETE -> to uncommit files

--- dist/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "🚀 Démarrage de SoccerBox..."
-echo "📍 Port: 3000"
-echo "🌐 URL: http://localhost:3000"
-echo "⏹️  Ctrl+C pour arrêter"
-echo ""
-cd "$(dirname "$0")"
-node server.js
